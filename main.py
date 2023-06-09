27a511faba6abc59c75f8da168be92b72b043082
Sulav Baral <sulavbaral58@gmail.com>
2023-06-09 15:58:48 +0545

    helo

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
-from flask import Flask, jsonify
-import os
-
+from flask import Flask, render_template, jsonify
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def home():
+    return render_template('./index.html')
+
+
+@app.route("/toc")
+def toc():
+    return render_template('./toc.html')
+
+
+@app.route("/os")
+def os():
+    return render_template('./os.html')
+
+
+@app.route("/dbms")
+def dbms():
+    return render_template('./dbms.html')
+
+
+@app.route("/cn")
+def cn():
+    return render_template('./cn.html')
+
+
+@app.route('/ai')
+def ai():
+    return render_template('./ai.html')
+
+
+@app.route('/questions/<subject>')
+def get_questions(subject):
+    print(subject)
+    with open('./json/' + subject + '.json') as file:
+        questions_data = json.load(file)
+    return jsonify(questions_data)
+
+
+@app.route("/scrape", methods=['GET'])
+def scrape():
+    # scraper = WebScraper()
+    # scraper.scrape_options()
+    return "Scraping done!"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
