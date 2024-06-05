9262df4be20dd589feb1e2f3eb131a02971789c8
tyzrex <sulavbaral58@gmail.com>
2024-06-05 17:53:58 +0545

    clean subject names

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def home():
 
 @app.route("/unitwisequestions/<subject>")
 def get_unitwise_questions(subject):
-    print(subject)
-    with open(f"questions_json/{subject.replace(" ",'')}.json") as file:
+    clean_subject = subject.replace(" ", "")
+    with open(f"questions_json/{clean_subject}.json") as file:
         questions_data = json.load(file)
     return jsonify(questions_data)
 
